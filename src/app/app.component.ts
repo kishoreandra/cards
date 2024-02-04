4a2c62c05750fe456730ee5911643e58999b9d4d
Kishore Kumar Andra <50901558+kishoreandra@users.noreply.github.com>
2024-02-04 23:34:34 +0530

    ng card app - stephen grider U

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org',
+      imageurl: 'https://picsum.photos/300?12',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      website: 'anastasia.net',
+      imageurl: 'https://picsum.photos/300?121',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      phone: '1-463-123-4447',
+      website: 'ramiro.info',
+      imageurl: 'https://picsum.photos/300?123',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      website: 'kale.biz',
+      imageurl: 'https://picsum.photos/300?125',
+    },
+  ];
   title = 'cards';
 }
